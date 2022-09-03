bf4465cdf50e4b27839399bf80a38fd9a5dd4b6f
tditada <tditada@gmail.com>
2022-09-03 21:04:59 +0200

    feat: gallery with dogs api

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,30 +1,61 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
-// import Box from '@mui/material/Box';
+import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-// import InfiniteScroll from "react-infinite-scroll-component";
+import CircularProgress from '@mui/material/CircularProgress';
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const StyledBox = styled(Box)`
+  margin: 3em;
+`
 
 const BREED_IMAGES_API = (breed: string): string => `https://dog.ceo/api/breed/${breed}/images`
+
+const SCROLL_DATA_SIZE = 6;
 
 type GetBreedType = {
   message: Array<string>,
   success: string,
 }
 
-function BreedGallery() {
+// TODO: empty state, errors
+function BreedGallery({ breed }: { breed: string | undefined }) {
   const [images, setImages] = useState<Array<string>>([]);
-  const [currImgs, setCurrImgs] = useState<Array<string>>([]);
   const [isLoading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>();
+
+  const [scrollData, setScrollData] = useState<Array<string>>([]);
+  const [hasMoreValue, setHasMoreValue] = useState<boolean>(true);
+
+  const loadScrollData = async () => {
+    try {
+      setScrollData(images.slice(0, scrollData.length + SCROLL_DATA_SIZE));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleOnRowsScrollEnd = () => {
+    if (scrollData.length < images.length) {
+      setHasMoreValue(true);
+      loadScrollData();
+    } else {
+      setHasMoreValue(false);
+    }
+  };
 
   const getData = useCallback(async () => {
     try {
       setLoading(true);
-      const { data } = await axios.get<GetBreedType>(BREED_IMAGES_API('hound'));
-      setImages(() => data.message);
-      setError(null);
+      if (breed) {
+        const { data } = await axios.get<GetBreedType>(BREED_IMAGES_API(breed));
+        setImages(data.message);
+        setScrollData(data.message.slice(0, SCROLL_DATA_SIZE));
+        setError(null);
+      }
     } catch (err: any) {
       setError(err.message);
       setImages([]);
@@ -36,37 +67,37 @@ function BreedGallery() {
 
   useEffect(() => {
     getData();
-    setCurrImgs(images.slice(0,10));
- }, [getData, images]);
+  }, [getData]);
 
   return (
-      <div>
-        {/* <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}> */}
-          <ImageList sx={{ width: 500, height: 450 }} rowHeight={164}>
-            {/* <InfiniteScroll
-              dataLength={currImgs.length}
-              next={() => {}}
-              hasMore={true}
-              loader={<h4>Loading...</h4>}
-              scrollableTarget= "scrolableDiv"
-            > */}
-              {currImgs.map((dogUrl: string) => (
+    <div>
+      <StyledBox >
+        <InfiniteScroll
+          dataLength={scrollData.length}
+          next={handleOnRowsScrollEnd}
+          hasMore={hasMoreValue}
+          scrollThreshold={1}
+          loader={<CircularProgress />}
+          style={{ overflow: "unset" }}
+        >
+          {isLoading ?
+            <CircularProgress data-testid="circular-progress" /> :
+            (<ImageList>
+              {scrollData.map((dogUrl: string) => (
                 <ImageListItem key={dogUrl}>
                   <img
-                    src={`${dogUrl}?w=248&fit=crop&auto=format`}
-                    srcSet={`${dogUrl}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    src={`${dogUrl}?w=124&fit=crop&auto=format`}
+                    srcSet={`${dogUrl}?w=124&fit=crop&auto=format&dpr=2 2x`}
                     alt={'dog'}
                     loading="lazy"
-                    // style={{ minHeight: 10 }}
                   />
                 </ImageListItem>
               ))}
-            {/* </InfiniteScroll> */}
-          </ImageList>
-        {/* </Box> */}
-        {error}
-        {isLoading}
-      </div>
+            </ImageList>)}
+        </InfiniteScroll>
+      </StyledBox>
+      {error}
+    </div>
   );
 }
 
